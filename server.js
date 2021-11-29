890b445ce5f52480386ed2c7cfe54aaa24863b60
Lyss Garcia <alyssa.g200@gmail.com>
2021-11-28 20:02:32 -0800

    edited boilerplate

--- server.js
+++ server.js
@@ -2,11 +2,15 @@
 const express = require("express");
 const path = require("path");
 
+// Express listening on port
 const app = express();
-
-// Static folder
-app.use(express.static(path.join(__dirname, 'public')))
-
 const PORT = process.env.PORT || 3001
 
+// Sts up data parsing using Express
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+let notes = require("./db/db.json")
+
+// Server listening on port
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
