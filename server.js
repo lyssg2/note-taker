ed3013832607eaeb871939edae00fcce563ddf48
Lyss Garcia <alyssa.g200@gmail.com>
2021-11-28 20:55:21 -0800

    required routes

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 // Dependencies
 const express = require("express");
 const path = require("path");
+const fs = require("fs")
 
 // Express listening on port
 const app = express();
 const PORT = process.env.PORT || 3001
 
-// Sts up data parsing using Express
-app.use(express.static("public"))
+// Sets up data parsing using Express
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-let notes = require("./db/db.json")
+
+// Requires routes
+require('./routes/apiRoutes')(app)
+require('./routes/htmlRoutes')
+
+// Directs to use public folder
+app.use(express.static("public"))
 
 // Server listening on port
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
