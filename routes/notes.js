2327c8c41a9558e5c344dca9677d97b30c91bd98
Lyss Garcia <alyssa.g200@gmail.com>
2021-12-02 19:02:17 -0800

    cleaned up code, removed unnecessary files

--- routes/notes.js
+++ routes/notes.js
@@ -4,7 +4,6 @@ const router = express.Router()
 const data = require('../db/db.json');
 const { v4: uuidv4 } = require('uuid')
 const fs = require('fs')
-const path = require('path')
 
 // GET request for notes
 router.get('/', (req, res) => {
